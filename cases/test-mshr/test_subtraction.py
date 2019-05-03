2bc20b469eb2b49fdd2663377902b7240c2d8e67
Lambert Theisen <lambert.theisen@rwth-aachen.de>
2019-05-03 12:07:47 +0200

    Include max edge length to mshr test

--- cases/test-mshr/test_subtraction.py
+++ cases/test-mshr/test_subtraction.py
@@ -7,9 +7,11 @@ def main():
     import dolfin as d
     import matplotlib.pyplot as plt
 
-    r_1 = 0.5 # inner radius
-    r_2 = 2.0 # outer radisu
-    res = 100 # resolution
+    d.set_log_level(13) # PROGRESS
+
+    r_1 = 0.5 # inner
+    r_2 = 2.0 # outer
+    res = 10 # resolution
 
     circle_inner = m.Circle(d.Point(0.0, 0.0), r_1)
     circle_outer = m.Circle(d.Point(0.0, 0.0), r_2)
@@ -20,6 +22,8 @@ def main():
     domain.set_subdomain(2, circle_outer)
 
     mesh = m.generate_mesh(domain, res)
+
+    print("max edge length:", mesh.hmax())
 
     mesh_file_pvd = d.File("mesh.pvd")
     mesh_file_pvd.write(mesh)
