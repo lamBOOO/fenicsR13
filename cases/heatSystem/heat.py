d7b10cbad394cbe35265d89d69b1b5be129f86d4
Lambert Theisen <lambert.theisen@rwth-aachen.de>
2019-05-10 17:52:20 +0200

    Refactor, remove Dirichlet and add weak boundary conditions

--- cases/heatSystem/heat.py
+++ cases/heatSystem/heat.py
@@ -1,17 +1,43 @@
 #!/usr/bin/env python3
 # pylint: disable=invalid-name
 
-"""
-Program to solve the heat system
-"""
-__author__ = "L. Theisen"
-__copyright__ = "2019 %s" % __author__
 
+# ------------------------------------------------------------------------------
+# PYLINT SETTINGS
+# ------------------------------------------------------------------------------
+# none
+# ------------------------------------------------------------------------------
+
+
+"""
+Program to solve the decoupled (removed coupling term) heat system of the linearized R13 equations
+"""
+
+
+# ------------------------------------------------------------------------------
+# IMPORTS
+# ------------------------------------------------------------------------------
 import matplotlib.pyplot as plt
 import dolfin as d
 d.set_log_level(1)  # all logs
+# ------------------------------------------------------------------------------
 
-# Load mesh and setup spaces
+
+
+# ------------------------------------------------------------------------------
+# SETTINGS
+# ------------------------------------------------------------------------------
+
+# Continous Interior Penalty (CIP) Stabilization with parameter delta_1:
+stab_cip = True
+delta_1 = d.Constant(1.0)
+
+# ------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
+# SETUP COMPUTATIONAL DOMAIN
+# ------------------------------------------------------------------------------
 mesh = d.Mesh("ring.xml") # TODO: Include internal mesher
 mesh_regions = d.MeshFunction('size_t', mesh, "ring_physical_region.xml")
 mesh_bounds = d.MeshFunction('size_t', mesh, "ring_facet_region.xml")
@@ -20,16 +46,24 @@ plt.figure()
 d.plot(mesh, title="Mesh")
 plt.draw()
 print("Max edge length:", mesh.hmax())
+# ------------------------------------------------------------------------------
 
+
+# ------------------------------------------------------------------------------
 # Setup function spaces and shape functions
+# ------------------------------------------------------------------------------
 el_p2 = d.VectorElement("Lagrange", mesh.ufl_cell(), 2)
 el_p1 = d.FiniteElement("Lagrange", mesh.ufl_cell(), 1)
 el_mxd = d.MixedElement([el_p2, el_p1])
 v_p2 = d.FunctionSpace(mesh, el_p2)
 v_p1 = d.FunctionSpace(mesh, el_p1)
 w = d.FunctionSpace(mesh, el_mxd)
+# ------------------------------------------------------------------------------
 
+
+# ------------------------------------------------------------------------------
 # Boundary conditions
+# ------------------------------------------------------------------------------
 # FIXME: Insert the right BC
 theta_inner = d.Constant(1.0)
 theta_outer = d.Constant(0.5)
@@ -49,13 +83,18 @@ bc_s_outer = d.DirichletBC(w.sub(0), s_outer, mesh_bounds, 3100)
 bcs_theta = [bc_theta_inner, bc_theta_outer]
 bcs_s = [bc_s_inner, bc_s_outer]
 # bcs_full = bcs_theta + bcs_s
-bcs = bcs_theta
+# bcs = bcs_theta
+bcs = []
+# ------------------------------------------------------------------------------
 
+
+# ------------------------------------------------------------------------------
 # Setup problem definition
+# ------------------------------------------------------------------------------
 tau = d.Constant(0.1)
-xi_tilde = d.Constant(0.1) # FIXME: Lookup right values
-# THETA_W = d.Constant(0.5) # FIXME: Implement BC
-delta_1 = d.Constant(1.0)
+xi_tilde = d.Constant(1.0) # FIXME: Lookup right value
+theta_w_inner = d.Constant(1.0)
+theta_w_outer = d.Constant(0.5)
 
 # Define trial and testfunction
 (s, theta) = d.TrialFunctions(w)
@@ -63,28 +102,40 @@ delta_1 = d.Constant(1.0)
 
 # Define source function
 f = d.Expression("2 - pow(x[0],2) - pow(x[1],2)", degree=2)
-# F = d.Expression("0", degree=0)
 
 print("Setup variational formulation")
 
+# Normal and tangential components
+# tangential (tx,ty) = (-ny,nx) only for 2D
+
+# Define custom measeasure for boundaries
+ds = d.Measure('ds', domain=mesh, subdomain_data=mesh_bounds)
+
 n = d.FacetNormal(mesh)
-s_n = d.dot(s, n)
-r_n = d.dot(r, n)
-s_t = d.sqrt(d.dot(s, s) - s_n)
-r_t = d.sqrt(d.dot(r, r) - r_n)
+s_n = s[0] * n[0] + s[1] * n[1]
+r_n = r[0] * n[0] + r[1] * n[1]
+s_t = - s[0] * n[1] + s[1] * n[0]
+r_t = - r[0] * n[1] + r[1] * n[0]
 
 a1 = (
     + 12/5 * tau * d.inner(d.dev(d.grad(s)), d.grad(r))
     + 2/3 * 1/tau * d.dot(s, r)
     - 5/2 * theta * d.div(r)
-    ) * d.dx
+    ) * d.dx + (
+        + 5/(4*xi_tilde) * s_n * r_n
+        + 11/10 * xi_tilde * s_t * r_t
+    ) * d.ds
 a2 = - (d.div(s) * kappa) * d.dx
-stab_cip = - (delta_1 * d.jump(d.grad(theta), n)
-              * d.jump(d.grad(kappa), n)) * d.dS
-l1 = 0
+l1 = - 5/2 * r_n * theta_w_inner * ds(3000) - 5/2 * r_n * theta_w_outer * ds(3100)
 l2 = - (f * kappa) * d.dx
 
-a = a1 + a2 + stab_cip
+if stab_cip:
+    stab = - (delta_1 * d.jump(d.grad(theta), n)
+              * d.jump(d.grad(kappa), n)) * d.dS
+else:
+    stab = 0
+
+a = a1 + a2 + stab
 l = l1 + l2
 
 # FIXME: Add edge scaling to CIP term
@@ -93,7 +144,12 @@ l = l1 + l2
 #   + (11*XI_TILDE)/10 * S_T * R_T)*ds
 # FIXME: Include jump term, see MS thesis from sweden: "jump"
 # FIXME: L += (5/2 * THETA_W) * R_N * ds
+# ------------------------------------------------------------------------------
 
+
+# ------------------------------------------------------------------------------
+# SOLVE THE SYSTEM
+# ------------------------------------------------------------------------------
 print("Solving system...")
 sol = d.Function(w)
 d.solve(a == l, sol, bcs)
@@ -115,15 +171,18 @@ d.plot(s, title="s")
 plt.figure()
 d.plot(theta, title="theta")
 plt.show()
+# ------------------------------------------------------------------------------
 
+
+# ------------------------------------------------------------------------------
+# COMPARE WITH EXACT SOLUTION
+# ------------------------------------------------------------------------------
 # Exact solution and L_2/L_inf errors, high degree for good quadr.
 # X0 = d.FunctionSpace(mesh, "Lagrange", 1)
-r = d.Expression("sqrt(pow(x[0],2)+pow(x[1],2))", degree=5)
-c1 = d.Expression("-0.40855716127979214", degree=5)
-c2 = d.Expression("2.4471587630476663", degree=5)
-theta_e = d.Expression("""
-    (-20*c1*log2(r) + (5*pow(r,4))/4 - 2*pow(r,2)*(24*pow(tau,2)+5))/(75*tau)
-    + c2 """, degree=5, r=r, tau=tau, c1=c1, c2=c2)
+R = d.Expression("sqrt(pow(x[0],2)+pow(x[1],2))", degree=50)
+C_1 = d.Expression("-0.40855716127979214", degree=5)
+C_2 = d.Expression("2.4471587630476663", degree=5)
+theta_e = d.Expression(""" C_2 + (1.0/75.0)*(-20*C_1*std::log(R) + (5.0/4.0)*std::pow(R, 4) - 2*std::pow(R, 2)*(24*std::pow(tau, 2) + 5))/tau """, degree=5, R=R, tau=tau, C_1=C_1, C_2=C_2)
 theta_e = d.interpolate(theta_e, v_p1)
 err_l2 = d.errornorm(theta_e, theta, 'L2')
 print("L2 error:", err_l2)
@@ -134,3 +193,4 @@ plt.show()
 theta_e.rename('theta_e', 'theta_e')
 file_theta_e = d.File("theta_e.pvd")
 file_theta_e.write(theta_e)
+# ------------------------------------------------------------------------------
