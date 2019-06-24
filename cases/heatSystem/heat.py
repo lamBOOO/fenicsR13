ee0eaca0a7c516060d05319267946908cc569467
Lambert Theisen <lambert.theisen@rwth-aachen.de>
2019-06-24 22:48:26 +0200

    Refactor and remove system 3

--- cases/heatSystem/heat.py
+++ cases/heatSystem/heat.py
@@ -6,7 +6,8 @@
 # DOCUMENTATION
 # **************************************************************************** #
 """
-Program to solve the decoupled (removed coupling term) heat system of the linearized R13 equations
+Program to solve the decoupled (removed coupling term) heat system of the
+linearized R13 equations
 """
 # **************************************************************************** #
 
@@ -55,8 +56,8 @@ d.set_log_level(1000)  # 1: all logs
 # **************************************************************************** #
 
 # Problem parameters
-system_ = "2" # 1=westerkamp2019, 2=coefficientless
-tau_ = "100.0" # float
+system_ = "1" # 1=westerkamp2019, 2=coefficientless
+tau_ = "0.1" # float
 A0_ = "2" # int
 A1_ = "0" # int
 A2_ = "-1" # int
@@ -110,12 +111,16 @@ def create_mesh(p, plot_mesh_=False, overwrite_=False):
         geo_name = "ring"
         mesh_name = "{}{}".format(geo_name, p)
 
-        mesh_avail = os.path.isfile(mesh_name + ".xml") and os.path.isfile(
-            mesh_name + "_facet_region.xml") and os.path.isfile(mesh_name + "_physical_region.xml")
+        mesh_avail = (
+            os.path.isfile(mesh_name + ".xml") and
+            os.path.isfile(mesh_name + "_facet_region.xml") and
+            os.path.isfile(mesh_name + "_physical_region.xml")
+        )
 
         if not mesh_avail or overwrite_:
             os.system(
-                "{} -setnumber p {} -2 -o {}.msh {}.geo".format(gmsh_path, p, mesh_name, geo_name))
+                "{} -setnumber p {} -2 -o {}.msh {}.geo".format(
+                    gmsh_path, p, mesh_name, geo_name))
             os.system("dolfin-convert {0}.msh {0}.xml".format(mesh_name))
 
         mesh_ = d.Mesh("{}.xml".format(mesh_name))
@@ -159,7 +164,7 @@ def setup_function_spaces_heat(mesh_, deg_s_, deg_theta_):
 # **************************************************************************** #
 # Setup problem
 # **************************************************************************** #
-def setup_variational_formulation_heat(w_, v_scalar_, mesh_, mesh_bounds_):
+def setup_variational_form_heat(w_, v_scalar_, mesh_, mesh_bounds_):
     """
     xi_tilde normally d.sqrt(2/d.pi), but we use 1 that looks right
     Note: Sign of a2 and l2 are correlated to sign of cip stabilization!!
@@ -185,14 +190,18 @@ def setup_variational_formulation_heat(w_, v_scalar_, mesh_, mesh_bounds_):
     # Define source function
     R = d.Expression("sqrt(pow(x[0],2)+pow(x[1],2))", degree=2)
     phi = d.Expression("atan2(x[1],x[0])", degree=2)
-    f = d.Expression("A0 + A2 * pow(R,2) + A1 * cos(phi)", degree=2, R=R, phi=phi, A0=A0, A1=A1, A2=A2)
+    f_str = "A0 + A2 * pow(R,2) + A1 * cos(phi)"
+    f = d.Expression(f_str, degree=2, R=R, phi=phi, A0=A0, A1=A1, A2=A2)
     f_i = d.interpolate(f, v_scalar_)
     file_f = d.File("f.pvd")
     file_f.write(f_i)
 
+    dev_grad_s = (0.5*(d.grad(s_)+u.transpose(d.grad(s_)))
+                  - (1/3)*u.tr(d.grad(s_))*u.Identity(2))
+
     if system == 1:
         a1 = (
-            + 12/5 * tau * d.inner(0.5*(d.grad(s_)+u.transpose(d.grad(s_)))-(1/3)*u.tr(d.grad(s_))*u.Identity(2), d.grad(r_))
+            + 12/5 * tau * d.inner(dev_grad_s, d.grad(r_))
             + 2/3 * (1/tau) * d.inner(s_, r_)
             - (5/2) * theta_ * d.div(r_)
         ) * d.dx + (
@@ -200,11 +209,12 @@ def setup_variational_formulation_heat(w_, v_scalar_, mesh_, mesh_bounds_):
             + 11/10 * xi_tilde * s_t * r_t
         ) * d.ds
         a2 = - (d.div(s_) * kappa_) * d.dx
-        l1 = - 5.0/2.0 * r_n * theta_w_outer * d.ds(3100) - 5.0/2.0 * r_n * theta_w_inner * d.ds(3000)
+        l1 = (- 5.0/2.0 * r_n * theta_w_outer * d.ds(3100)
+              - 5.0/2.0 * r_n * theta_w_inner * d.ds(3000))
         l2 = - (f * kappa_) * d.dx
     elif system == 2:
         a1 = (
-            tau * d.inner(0.5*(d.grad(s_)+u.transpose(d.grad(s_)))-(1/3)*u.tr(d.grad(s_))*u.Identity(2), d.grad(r_))
+            tau * d.inner(dev_grad_s, d.grad(r_))
             + (1/tau) * d.inner(s_, r_)
             - theta_ * d.div(r_)
         ) * d.dx + (
@@ -212,17 +222,8 @@ def setup_variational_formulation_heat(w_, v_scalar_, mesh_, mesh_bounds_):
             + xi_tilde * s_t * r_t
         ) * d.ds
         a2 = - (d.div(s_) * kappa_) * d.dx
-        l1 = - r_n * theta_w_outer * d.ds(3100) - r_n * theta_w_inner * d.ds(3000)
-        l2 = - (f * kappa_) * d.dx
-    elif system == 3:
-        a1 = (
-            (1/tau) * d.inner(s_, r_)
-            - theta_ * d.div(r_)
-        ) * d.dx + (
-            + 1/(xi_tilde) * s_n * r_n
-        ) * d.ds
-        a2 = - (d.div(s_) * kappa_) * d.dx
-        l1 = - r_n * theta_w_outer * d.ds(3100) -  r_n * theta_w_inner * d.ds(3000)
+        l1 = (- r_n * theta_w_outer * d.ds(3100)
+              - r_n * theta_w_inner * d.ds(3000))
         l2 = - (f * kappa_) * d.dx
     else:
         raise Exception('system={} is undefined'.format(system))
@@ -233,10 +234,12 @@ def setup_variational_formulation_heat(w_, v_scalar_, mesh_, mesh_bounds_):
         h_avg = mesh_.hmax()
 
         # 2)
+        # TODO: Test this
         # h = d.CellDiameter(mesh_)
         # h_avg = (h('+') + h('-'))/2.0
 
-        stab = - (delta_1 * h_avg**3 * d.jump(d.grad(theta_), n) * d.jump(d.grad(kappa_), n)) * d.dS
+        stab = - (delta_1 * h_avg**3 * d.jump(d.grad(theta_), n)
+                  * d.jump(d.grad(kappa_), n)) * d.dS
     else:
         stab = 0
 
@@ -319,10 +322,10 @@ def get_exact_solution_heat():
                 item.get("theta_w_outer") == theta_w_outer_):
             R = d.Expression("sqrt(pow(x[0],2)+pow(x[1],2))", degree=2)
             phi = d.Expression("atan2(x[1],x[0])", degree=2)
-            theta_e = d.Expression(item.get("theta_e"), degree=2, R=R,tau=tau)
-            s_R = d.Expression(item.get("s_R"), degree=2, R=R,tau=tau)
+            theta_e = d.Expression(item.get("theta_e"), degree=2, R=R, tau=tau)
+            s_R = d.Expression(item.get("s_R"), degree=2, R=R, tau=tau)
             s_e = d.Expression(("s_R * cos(phi)", "s_R * sin(phi)"),
-                            degree=2, phi=phi, s_R=s_R)
+                               degree=2, phi=phi, s_R=s_R)
             return (s_e, theta_e)
 
     # no exact solution is csv file
@@ -382,8 +385,12 @@ def calc_vectorfield_errors(sol_, sol_e_, v_sol, mesh_, name_, p_):
     file_difference.write(difference)
 
     dim = field_i.geometric_dimension()
-    errs_f_L2 = [d.errornorm(field_e_i.split()[i], field_i.split()[i], 'L2', mesh=mesh_) for i in range(dim)]
-    errs_v_linf = [d.norm(field_e_i.split()[i].vector()-field_i.split()[i].vector(), 'linf') for i in range(dim)]
+    errs_f_L2 = [d.errornorm(
+        field_e_i.split()[i], field_i.split()[i], 'L2', mesh=mesh_
+    ) for i in range(dim)]
+    errs_v_linf = [d.norm(
+        field_e_i.split()[i].vector()-field_i.split()[i].vector(), 'linf'
+    ) for i in range(dim)]
     print("L_2 error:", errs_f_L2)
     print("l_inf error:", errs_v_linf)
 
@@ -452,7 +459,7 @@ def solve_system_heat():
     for p in range(max_exponent):
         (mesh, _, mesh_bounds) = create_mesh(p)
         (w, v_s, v_theta) = setup_function_spaces_heat(mesh, deg_s, deg_theta)
-        (a, l) = setup_variational_formulation_heat(w, v_theta, mesh, mesh_bounds)
+        (a, l) = setup_variational_form_heat(w, v_theta, mesh, mesh_bounds)
         (s, theta) = solve_variational_formulation_heat(a, l, w, [])
         (s_e, theta_e) = get_exact_solution_heat()
 
@@ -460,7 +467,9 @@ def solve_system_heat():
         theta_fspaces.append(v_theta)
         if not p == 0:
             d.Function.set_allow_extrapolation(theta_array[p], True)
-            theta_l2_change.append(d.errornorm(d.project(theta_array[p], theta_fspaces[p-1]), theta_array[p-1]))
+            theta_l2_change.append(d.errornorm(
+                d.project(theta_array[p], theta_fspaces[p-1]), theta_array[p-1]
+            ))
 
         data_sx["p"].append(p)
         data_sx["h"].append(mesh.hmax())
@@ -484,7 +493,8 @@ def solve_system_heat():
     plot_errors(data_sx, "sx")
     plot_errors(data_sy, "sy")
     plot_errors(data_theta, "Theta")
-    plot_single(data_sx["h"][:-1], theta_l2_change, "norm(theta_i-theta_{i-1})_L2", "theta change")
+    plot_single(data_sx["h"][:-1], theta_l2_change,
+                "norm(theta_i-theta_{i-1})_L2", "theta change")
 # **************************************************************************** #
 
 
