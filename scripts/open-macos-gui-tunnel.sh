8986ef738d675d58eb0ab6fdbbdbe76afb497ce6
Lambert Theisen <lambert.theisen@rwth-aachen.de>
2019-07-07 13:21:32 +0200

    Update gui tunnel script for osx

--- scripts/open-macos-gui-tunnel.sh
+++ scripts/open-macos-gui-tunnel.sh
@@ -2,6 +2,8 @@
 
 # http://joshuamccall.com/articles/docker.html
 open -a XQuartz
-export ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+export ip0=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+export ip1=$(ifconfig en1 | grep inet | awk '$1=="inet" {print $2}')
+export ip=$ip0$ip1
 export TEST=hi
 xhost + $ip
