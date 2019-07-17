2d92e8ecb2ae6a7a1f1db9bdcbf10e5e854a8f23
Lambert Theisen <lambert.theisen@rwth-aachen.de>
2019-07-17 11:51:30 +0000

    Fix heat pytest to build meshes

--- fenicsR13/tests/heat/test_heat_convergence.py
+++ fenicsR13/tests/heat/test_heat_convergence.py
@@ -1,11 +1,15 @@
 "Test for convergence"
 
+import subprocess
+
 def test_heat_convergence():
     """
     Executes westerkamp2019 decoupled heat system test and check with
     reference errors
     """
-    import subprocess
+
+    subprocess.call(["python3", "create_meshes.py"], cwd="tests/mesh")
+
     subprocess.call(["python3", "../../src/fenicsR13.py"], cwd="tests/heat")
 
     # Check against reference errors, return exception if diff returns with !=0
