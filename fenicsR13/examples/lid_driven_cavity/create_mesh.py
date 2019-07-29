22480695b542742d5ea9e77d71e7a9bdff182716
Lambert Theisen <lambert.theisen@rwth-aachen.de>
2019-07-29 22:00:54 +0000

    Add lid driven cavity as first example
    - Can maybe used later to compare against torrilhon2013

---
+++ fenicsR13/examples/lid_driven_cavity/create_mesh.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+"Script to generate a set of ring meshes"
+
+import os
+import dolfin as df
+
+GMSH_PATH = "gmsh"
+GEO_NAME = "lid"
+
+def create_mesh(exponent):
+    "Generates a mesh using gmsh"
+
+    mesh_name = "{}{}".format(GEO_NAME, exponent)
+
+    os.system(
+        "{} -setnumber p {} -2 -o {}.msh {}.geo".format(
+            GMSH_PATH, exponent, mesh_name, GEO_NAME))
+
+    os.system("dolfin-convert {0}.msh {0}.xml".format(mesh_name))
+
+    mesh = df.Mesh("{}.xml".format(mesh_name))
+    subdomains = df.MeshFunction(
+        "size_t", mesh, "{}_physical_region.xml".format(mesh_name))
+    boundaries = df.MeshFunction(
+        "size_t", mesh, "{}_facet_region.xml".format(mesh_name))
+
+    file = df.HDF5File(mesh.mpi_comm(), "{}.h5".format(mesh_name), "w")
+    file.write(mesh, "/mesh")
+    file.write(subdomains, "/subdomains")
+    file.write(boundaries, "/boundaries")
+
+    return (mesh, subdomains, boundaries)
+
+for p in range(0+1, 5+1):
+    create_mesh(p)
