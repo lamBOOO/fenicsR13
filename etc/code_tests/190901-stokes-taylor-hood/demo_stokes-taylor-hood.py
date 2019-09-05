9531693a7623f4b466d00a47e320f2e6373d9831
Lambert Theisen <lambert.theisen@rwth-aachen.de>
2019-09-05 12:12:22 +0000

    Change Stokes demo

--- etc/code_tests/190901-stokes-taylor-hood/demo_stokes-taylor-hood.py
+++ etc/code_tests/190901-stokes-taylor-hood/demo_stokes-taylor-hood.py
@@ -1,47 +1,8 @@
-"""This demo solves the Stokes equations, using quadratic elements for
-the velocity and first degree elements for the pressure (Taylor-Hood
-elements). The sub domains for the different boundary conditions
-used in this simulation are computed by the demo program in
-src/demo/mesh/subdomains."""
-
-# Copyright (C) 2007 Kristian B. Oelgaard
-#
-# This file is part of DOLFIN.
-#
-# DOLFIN is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# DOLFIN is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
-#
-# Modified by Anders Logg, 2008-2009.
-#
-# First added:  2007-11-16
-# Last changed: 2009-11-26
-# Begin demo
-
-
 import matplotlib.pyplot as plt
 from dolfin import *
-
-
 # Load mesh and subdomains
-mesh = Mesh("../dolfin_fine.xml.gz")
-sub_domains = MeshFunction("size_t", mesh, "../dolfin_fine_subdomains.xml.gz")
-
-
-plt.figure()
-plot(mesh)
-
-plt.figure()
-plot(sub_domains)
+mesh = Mesh("dolfin_fine.xml.gz")
+sub_domains = MeshFunction("size_t", mesh, "dolfin_fine_subdomains.xml.gz")
 
 # Define function spaces
 P2 = VectorElement("Lagrange", mesh.ufl_cell(), 2)
@@ -53,12 +14,10 @@ W = FunctionSpace(mesh, TH)
 # x1 = 0, x1 = 1 and around the dolphin
 noslip = Constant((0, 0))
 bc0 = DirichletBC(W.sub(0), noslip, sub_domains, 0)
-
 # Inflow boundary condition for velocity
 # x0 = 1
 inflow = Expression(("-sin(x[1]*pi)", "0.0"), degree=2)
 bc1 = DirichletBC(W.sub(0), inflow, sub_domains, 1)
-
 # Collect boundary conditions
 bcs = [bc0, bc1]
 
@@ -68,21 +27,12 @@ bcs = [bc0, bc1]
 f = Constant((0, 0))
 a = (inner(grad(u), grad(v)) - div(v)*p + q*div(u))*dx
 L = inner(f, v)*dx
-
 # Compute solution
 w = Function(W)
 solve(a == L, w, bcs)
 
-# Split the mixed solution using deepcopy
-# (needed for further computation on coefficient vector)
-(u, p) = w.split(True)
-
-print("Norm of velocity coefficient vector: %.15g" % u.vector().norm("l2"))
-print("Norm of pressure coefficient vector: %.15g" % p.vector().norm("l2"))
-
 # # Split the mixed solution using a shallow copy
 (u, p) = w.split()
-
 # Save solution in VTK format
 ufile_pvd = File("velocity.pvd")
 ufile_pvd << u
@@ -92,9 +42,7 @@ pfile_pvd << p
 # Plot solution
 plt.figure()
 plot(u, title="velocity")
-
 plt.figure()
 plot(p, title="pressure")
-
 # Display plots
 plt.show()
