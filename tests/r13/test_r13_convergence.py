81645e33f7cae3942ad2a2562f9a4791e33a8f52
Lambert Theisen <lambert.theisen@rwth-aachen.de>
2020-08-17 22:28:17 +0200

    Add 3 more GLS tests for R13
    - Change tau-params by intuition ¯\_(ツ)_/¯

--- tests/r13/test_r13_convergence.py
+++ tests/r13/test_r13_convergence.py
@@ -167,6 +167,43 @@ class TestR13Convergence(object):
         referrors = "referrors/" + name + "/errors.csv"
         self.compare_errors(errors, referrors)
 
+    def test_r13_1_coeffs_nosources_norot_inflow_p2p2p2p2p2_gls(self):
+        r"""
+        Execute full linear R13 system test and check with reference errors.
+        Use second order equal elements.
+
+        Test case is similar to  [TOR2017]_.
+
+        .. [TOR2017] Torrilhon, M. et al. (2017). “Hierarchical Boltzmann
+            simulations and model error estimation”. In: Journal of
+            Computational Physics 342 (2017), pp. 66–84.
+
+        ==================== ===================================================
+        Parameter     Value
+        ==================== ===================================================
+        :math:`Kn`           :math:`1.0`
+        :math:`\dot{m}`      :math:`0`
+        :math:`r`            :math:`0`
+        :math:`\theta_w^1`   :math:`1.0`
+        :math:`v_t^1`        :math:`0`
+        :math:`v_n^1`        :math:`0`
+        :math:`p_w^1`        :math:`0`
+        :math:`\epsilon_w^1` :math:`10^{-3}`
+        :math:`\theta_w^2`   :math:`2.0`
+        :math:`v_t^2`        :math:`-1.00 \sin(\phi)`
+        :math:`v_n^2`        :math:`+1.00 \cos(\phi)`
+        :math:`p_w^2`        :math:`-0.27 \cos(\phi)`
+        :math:`\epsilon_w^2` :math:`10^{3}`
+        Elements             :math:`P_2P_2P_2P_2P_2`
+        Stabilization        GLS
+        ==================== ===================================================
+        """
+        name = "r13_1_coeffs_nosources_norot_inflow_p2p2p2p2p2_gls"
+        self.run_solver("inputs/" + name + ".yml")
+        errors = name + "/" + "errors.csv"
+        referrors = "referrors/" + name + "/errors.csv"
+        self.compare_errors(errors, referrors)
+
     def test_r13_1_coeffs_nosources_norot_inflow_p2p2p2p2p2_stab(self):
         r"""
         Execute full linear R13 system test and check with reference errors.
@@ -195,10 +232,47 @@ class TestR13Convergence(object):
         :math:`p_w^2`        :math:`-0.27 \cos(\phi)`
         :math:`\epsilon_w^2` :math:`10^{3}`
         Elements             :math:`P_2P_2P_2P_2P_2`
-        Stabilization        Off
+        Stabilization        CIP
         ==================== ===================================================
         """
         name = "r13_1_coeffs_nosources_norot_inflow_p2p2p2p2p2_stab"
+        self.run_solver("inputs/" + name + ".yml")
+        errors = name + "/" + "errors.csv"
+        referrors = "referrors/" + name + "/errors.csv"
+        self.compare_errors(errors, referrors)
+
+    def test_r13_1_coeffs_sources_rot_noinflow_p1p1p1p1p1_gls(self):
+        r"""
+        Execute full linear R13 system test and check with reference errors.
+
+        Test case is similar to  [WES2019]_.
+
+        .. [WES2019] A. Westerkamp and M. Torrilhon. “Finite Element Methods
+            for the Linear Regularized 13-Moment Equations Describing Slow
+            Rarefied Gas Flows”. In: Journal of Computational Physics 389
+            (2019).
+
+        ==================== ===================================================
+        Parameter     Value
+        ==================== ===================================================
+        :math:`Kn`           :math:`1.0`
+        :math:`\dot{m}`      :math:`(1-\frac{5R^2}{18{Kn}^2})\cos(\phi)`
+        :math:`r`            :math:`0`
+        :math:`\theta_w^1`   :math:`1.0`
+        :math:`v_t^1`        :math:`10.0`
+        :math:`v_n^1`        :math:`0`
+        :math:`p_w^1`        :math:`0`
+        :math:`\epsilon_w^1` :math:`0`
+        :math:`\theta_w^2`   :math:`0.5`
+        :math:`v_t^2`        :math:`0.0`
+        :math:`v_n^2`        :math:`0`
+        :math:`p_w^2`        :math:`0`
+        :math:`\epsilon_w^2` :math:`0`
+        Elements             :math:`P_1P_1P_1P_1P_1`
+        Stabilization        GLS
+        ==================== ===================================================
+        """
+        name = "r13_1_coeffs_sources_rot_noinflow_p1p1p1p1p1_gls"
         self.run_solver("inputs/" + name + ".yml")
         errors = name + "/" + "errors.csv"
         referrors = "referrors/" + name + "/errors.csv"
@@ -236,6 +310,38 @@ class TestR13Convergence(object):
         ==================== ===================================================
         """
         name = "r13_1_coeffs_sources_rot_noinflow_p1p1p1p1p1_stab"
+        self.run_solver("inputs/" + name + ".yml")
+        errors = name + "/" + "errors.csv"
+        referrors = "referrors/" + name + "/errors.csv"
+        self.compare_errors(errors, referrors)
+
+    def test_r13_1_coeffs_sources_rot_noinflow_p2p2p2p2p2_gls(self):
+        r"""
+        Execute full linear R13 system test and check with reference errors.
+
+        Test case is similar to  [WES2019]_.
+
+        ==================== ===================================================
+        Parameter     Value
+        ==================== ===================================================
+        :math:`Kn`           :math:`1.0`
+        :math:`\dot{m}`      :math:`(1-\frac{5R^2}{18{Kn}^2})\cos(\phi)`
+        :math:`r`            :math:`0`
+        :math:`\theta_w^1`   :math:`1.0`
+        :math:`v_t^1`        :math:`10.0`
+        :math:`v_n^1`        :math:`0`
+        :math:`p_w^1`        :math:`0`
+        :math:`\epsilon_w^1` :math:`0`
+        :math:`\theta_w^2`   :math:`0.5`
+        :math:`v_t^2`        :math:`0.0`
+        :math:`v_n^2`        :math:`0`
+        :math:`p_w^2`        :math:`0`
+        :math:`\epsilon_w^2` :math:`0`
+        Elements             :math:`P_2P_2P_2P_2P_2`
+        Stabilization        CIP: :math:`\delta_\theta,\delta_u=1,\delta_p=0.01`
+        ==================== ===================================================
+        """
+        name = "r13_1_coeffs_sources_rot_noinflow_p2p2p2p2p2_gls"
         self.run_solver("inputs/" + name + ".yml")
         errors = name + "/" + "errors.csv"
         referrors = "referrors/" + name + "/errors.csv"
