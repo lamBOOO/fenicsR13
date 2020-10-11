8a2dca653fb7b47d6684b45a41e21fd7a713ce44
Lambert Theisen <lambert.theisen@rwth-aachen.de>
2020-10-11 22:16:38 +0200

    Update study2

--- examples/thermal_edge_flow/create_mesh2.sh
+++ examples/thermal_edge_flow/create_mesh2.sh
@@ -4,7 +4,7 @@ for p1 in $(seq -3 -1 -3)
 do
   for p2 in $(seq -6 -1 -6)
   do
-    for p3 in $(seq -6 -1 -10)
+    for p3 in $(seq -6 -1 -11)
     do
       outname=study2_"$p1"_"$p2"_"$p3".h5
       echo $outname
