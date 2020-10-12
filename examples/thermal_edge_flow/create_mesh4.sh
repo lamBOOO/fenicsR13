f2f90a01de8fcca47277b9dfe0d256290440af7f
Lambert Theisen <lambert.theisen@rwth-aachen.de>
2020-10-12 09:10:21 +0200

    Add study4

---
+++ examples/thermal_edge_flow/create_mesh4.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for p1 in $(seq -3 -1 -3)
+do
+  for p2 in $(seq -6 -1 -6)
+  do
+    for p3 in $(seq -6 -1 -11)
+    do
+      outname=study4_"$p1"_"$p2"_"$p3".h5
+      echo $outname
+      geoToH5 beamchamber_unstructured_nonuniform_2.geo "$outname" "-setnumber p1 $p1 -setnumber p2 $p2 -setnumber p3 $p3"
+    done
+  done
+done
