e1e26249814c733ba0f4902a88028b989c9dad40
Lambert Theisen <lambert.theisen@rwth-aachen.de>
2020-10-14 18:22:13 +0200

    Fix postprocessing in knudsen paradox cases

--- examples/channel_flow_pressure/postprocessing.sh
+++ examples/channel_flow_pressure/postprocessing.sh
@@ -4,7 +4,7 @@ rm table.csv
 
 for KNUDSEN in 0.03125 0.0625 0.125 0.25 0.5 1.0 2.0
 do
-  MASSFLOW=$(cat results_channel_flow_pressure$KNUDSEN/massflow_3002)
+  MASSFLOW=$(cat results_channel_flow_pressure$KNUDSEN/massflow_3002_0)
   echo "$KNUDSEN, $MASSFLOW" >> table.csv
 done
 
