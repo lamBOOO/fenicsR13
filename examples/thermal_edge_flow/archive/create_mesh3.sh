4b009f83defd0425859138d0201416a1f1fffd3f
Lambert Theisen <lambert.theisen@rwth-aachen.de>
2022-01-10 14:50:51 +0100

    Remove thermal edge flow archive

--- examples/thermal_edge_flow/archive/create_mesh3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for split in $(seq 0 1 3)
-do
-  outname=study3_"$split".h5
-  echo $outname
-  geoToH5 beamchamber_structured_split.geo "$outname" "-setnumber split $split"
-done
--- examples/thermal_edge_flow/archive/create_mesh5.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-for split in $(seq 0 1 3)
-do
-  outname=study5_"$split".h5
-  echo $outname
-  geoToH5 beamchamber_hybrid_curved.geo "$outname" "-setnumber split $split"
-done
