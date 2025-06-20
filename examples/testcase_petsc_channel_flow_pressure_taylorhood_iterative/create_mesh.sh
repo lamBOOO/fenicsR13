152aec5aaddf762e9b2201d7441f8af3718ec487
Lambert Theisen <lambert.theisen@rwth-aachen.de>
2025-06-20 17:59:23 +0200

    make petsc testcase bigger (needs cluster exec)

--- examples/testcase_petsc_channel_flow_pressure_taylorhood_iterative/create_mesh.sh
+++ examples/testcase_petsc_channel_flow_pressure_taylorhood_iterative/create_mesh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for p in 8 9 10 11 12 13 14
+for p in 8 9 10 11 12 13 14 15
 do
   geoToH5 channel.geo channel"$p".h5 "-setnumber p $p"
 done
--- examples/testcase_petsc_channel_flow_pressure_taylorhood_iterative/run.sh
+++ examples/testcase_petsc_channel_flow_pressure_taylorhood_iterative/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for pair in "0 2" "1 4" "2 8" "3 16" "4 32"; do
+for pair in "0 2" "1 4" "2 8" "3 16" "4 32" "5 64" "6 128" "7 256"; do
   read -r i n <<< "$pair"   # splits on IFS (space by default)
   mpirun -n $n fenicsR13 ./input$i.yml
 done
