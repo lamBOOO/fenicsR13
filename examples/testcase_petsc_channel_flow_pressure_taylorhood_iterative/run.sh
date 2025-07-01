9d94910f761d8a2dbc6ddee098062cef4b2eb6fe
Lambert Theisen <lambert.theisen@rwth-aachen.de>
2025-07-01 15:39:22 +0200

    update 2d study naming also

--- examples/testcase_petsc_channel_flow_pressure_taylorhood_iterative/run.sh
+++ examples/testcase_petsc_channel_flow_pressure_taylorhood_iterative/run.sh
@@ -2,7 +2,7 @@
 
 for pair in "0 2" "1 4" "2 8" "3 16" "4 32" "5 64" "6 128" "7 256" "8 512" "9 1024"; do
   read -r i n <<< "$pair"   # splits on IFS (space by default)
-  mpirun -n $n fenicsR13 ./input$i.yml
+  mpirun -n $n fenicsR13 ./input_$i.yml
 done
 
 # cleanup for sending
