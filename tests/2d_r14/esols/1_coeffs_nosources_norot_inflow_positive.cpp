c66bcf598e31c2c2c0e3029127b9fc39a011f9d1
Lambert Theisen <lambert.theisen@rwth-aachen.de>
2026-06-15 17:58:58 +0200

    R14 converges (with generalized TaylorHood) and fixed Mathematica notebooks

--- tests/2d_r14/esols/1_coeffs_nosources_norot_inflow_positive.cpp
+++ tests/2d_r14/esols/1_coeffs_nosources_norot_inflow_positive.cpp
@@ -12,128 +12,41 @@ namespace py = pybind11;
 
 double kn = 1.0;
 
-// // Constants eps=10^-5
-// double C_0 = 1.088418574133493;
-// double C_1 = -0.4708784837854026;
-// double C_2 = -0.1060780078877844;
-// double C_3 = 0.6397363738946663;
-// double C_4 = -0.04268373030990083;
-// double C_5 = 0.1027291801998076;
-// double C_6 = -0.1867020667903523;
-// double C_7 = 2.864923360570221e-7;
-// double C_8 = 1.963244537638472;
-// double C_9 = 0.007704644954804194;
-// double CK_1 = -0.01841555365364274;
-// double CK_2 = 0.02647436311982035;
-// double CK_3 = -0.0722116290730638;
-// double CK_4 = 0.01263372155827893;
-// double CK_5 = 0.03846910453592139;
-// double CK_6 = 0.003998659067908465;
-// double CK_7 = 0.05469703377538136;
-// double CK_8 = 0.04448263813229264;
-// double CK_9 = -0.001607567940383826;
-// double CK_10 = 0.1781491089766695;
-
-// // Constants eps=10^-4
-// double C_0 = 1.088458465830652;
-// double C_1 = -0.4708795265589713;
-// double C_2 = -0.1060844454754048;
-// double C_3 = 0.6397268622237638;
-// double C_4 = -0.0426888328939176;
-// double C_5 = 0.1027285697835575;
-// double C_6 = -0.1867022145254646;
-// double C_7 = 2.864825757859002e-6;
-// double C_8 = 1.963244238669897;
-// double C_9 = 0.007703775736883896;
-// double CK_1 = -0.01841695056397639;
-// double CK_2 = 0.02647443224729624;
-// double CK_3 = -0.07220941121077909;
-// double CK_4 = 0.01263374077119675;
-// double CK_5 = 0.03846727512087732;
-// double CK_6 = 0.003998627065175332;
-// double CK_7 = 0.0547022288973836;
-// double CK_8 = 0.04447888927384885;
-// double CK_9 = -0.001604798505932295;
-// double CK_10 = 0.178170030606713;
-
-// // Constants eps=10^-3
-double C_0 = 0.;
-double C_1 = -1.013912644124032;
-double C_2 = -1.2677166806836977;
-double C_3 = 0.04992492338715168;
-double C_4 = 1.450672783912046;
-double C_5 = 0.;
-double C_6 = -0.5758748079393637;
-double C_7 = 0.10460960523903531;
-double C_8 = -0.08439250784359258;
-double C_9 = -0.05818511245891174;
-double C_10 = 0.044231601637359494;
-double C_11 = -0.12041762247811193;
-double CK_1 = -0.12330240809678536;
-double CK_2 = 2.672949922828857;
-double CK_3 = 0.01079637736731986;
-double CK_4 = -0.049087424652564535;
-double CK_5 = -0.0016650371020496696;
-double CK_6 = 0.6120973059811231;
-double CK_7 = 0.018332699081602767;
-double CK_8 = 0.049452314164954506;
-double CK_9 = -0.00002172097726484143;
-double CK_10 = 0.27405520113895676;
-double CK_11 = 0.0018031130578746815;
-double CK_12 = 0.06975633394673138;
-double CK_13 = 0.;
-double CK_14 = 0.;
-double CK_15 = -0.017056297688993596;
-double CK_16 = -0.12307639475900282;
-
-// // Constants eps=10^-2
-// double C_0 = 1.09277278324226;
-// double C_1 = -0.4709565408029582;
-// double C_2 = -0.1067733521869535;
-// double C_3 = 0.6386400415463009;
-// double C_4 = -0.04323806992636355;
-// double C_5 = 0.1026552684836559;
-// double C_6 = -0.1867184037607001;
-// double C_7 = 0.0002854059606868768;
-// double C_8 = 1.963211476840119;
-// double C_9 = 0.007609935703161957;
-// double CK_1 = -0.0185667325855968;
-// double CK_2 = 0.02647991106816222;
-// double CK_3 = -0.07196637153183492;
-// double CK_4 = 0.0126358461775725;
-// double CK_5 = 0.03826680259919404;
-// double CK_6 = 0.003995120114343059;
-// double CK_7 = 0.05526041387534725;
-// double CK_8 = 0.04406535024733006;
-// double CK_9 = -0.001300480668267124;
-// double CK_10 = 0.180421409499547;
-
-// // Constants eps=10^-1
-// double C_0 = 1.125921604594353;
-// double C_1 = -0.4686941217654443;
-// double C_2 = -0.1114820681020402;
-// double C_3 = 0.6256505450040041;
-// double C_4 = -0.04724969749859539;
-// double C_5 = 0.1015107112011764;
-// double C_6 = -0.1868598401676765;
-// double C_7 = 0.002753811809744579;
-// double C_8 = 1.962925254827631;
-// double C_9 = 0.006913904797024802;
-// double CK_1 = -0.01961423092232162;
-// double CK_2 = 0.0263626002451694;
-// double CK_3 = -0.06984306808715408;
-// double CK_4 = 0.01265423995026528;
-// double CK_5 = 0.03651538489775871;
-// double CK_6 = 0.003964481821802226;
-// double CK_7 = 0.05925618682225351;
-// double CK_8 = 0.04024172550289172;
-// double CK_9 = 0.001420920976733996;
-// double CK_10 = 0.1968170158183649;
-
-double lambda_1 = 0.6546536707079771;
+// Lambdas
+double lambda_1 = 0.5345224838248488;
 double lambda_2 = 0.9128709291752769;
 double lambda_3 = 1.224744871391589;
 double lambda_4 = 0.7453559924999299;
+
+// Constants
+double C_0 = 0.;
+double C_1 = -0.1477752433373134;
+double C_2 = -0.18478991652292415;
+double C_3 = 0.007616222250050322;
+double C_4 = 1.9613523915545736;
+double C_5 = 0.;
+double C_6 = -0.025990132046856542;
+double C_7 = -0.20544333345973814;
+double C_8 = 0.08546266837583295;
+double C_9 = -0.01965608692371418;
+double C_10 = -0.040261109257103515;
+double C_11 = 1.1399409455629295;
+double CK_1 = -0.12432069916127567;
+double CK_2 = 0.24749516983247868;
+double CK_3 = -0.02593994988782829;
+double CK_4 = -0.006941070313859298;
+double CK_5 = -0.009378323117905304;
+double CK_6 = 0.05358547329379231;
+double CK_7 = -0.06686833734375043;
+double CK_8 = 0.0005915756434353135;
+double CK_9 = -0.0019786615912372857;
+double CK_10 = 0.019027436471274847;
+double CK_11 = -0.013685132661250488;
+double CK_12 = 0.011110746687159737;
+double CK_13 = 0.;
+double CK_14 = 0.;
+double CK_15 = 0.045818412638178437;
+double CK_16 = 0.06590993098932302;
 
 double BI( int n, double x ) { return boost::math::cyl_bessel_i(n,x); }
 double BK( int n, double x ) { return boost::math::cyl_bessel_k(n,x); }
@@ -157,9 +70,10 @@ class Temperature : public dolfin::Expression {
         double R = sqrt(pow(x[0],2)+pow(x[1],2));
         double phi = atan2(x[1],x[0]);
 
+        // Temperature
         double c_0 = C_4 - (CK_1*I_0((lambda_1*R)/kn))/15. - (8*CK_5*I_0((lambda_2*R)/kn))/15. - (CK_2*K_0((lambda_1*R)/kn))/15. - (8*CK_6*K_0((lambda_2*R)/kn))/15. - (4*C_2*log(R/kn))/15.;
         double c = (C_9*kn)/R + (C_10*R)/kn - (CK_3*I_1((lambda_1*R)/kn))/15. - (16*CK_7*I_1((lambda_2*R)/kn))/15. - (CK_4*K_1((lambda_1*R)/kn))/15. - (16*CK_8*K_1((lambda_2*R)/kn))/15.;
-
+        
         double theta = c_0 + cos(phi) * c;
 
         values[0] = theta;
@@ -177,7 +91,6 @@ class Heatflux : public dolfin::Expression {
 
         double alpha_0 = (C_2*kn)/R;
         double alpha = (-15*C_10)/4. - 3*C_8 - (1.5*C_7*pow(kn,2))/pow(R,2) + (3.75*C_9*pow(kn,2))/pow(R,2) + CK_15*(I_0((lambda_4*R)/kn) - I_2((lambda_4*R)/kn)) + CK_16*(K_0((lambda_4*R)/kn) - K_2((lambda_4*R)/kn));
-
         double beta_0 = CK_13*I_1((lambda_4*R)/kn) + CK_14*K_1((lambda_4*R)/kn);
         double beta = (-15*C_10)/4. - 3*C_8 + (1.5*C_7*pow(kn,2))/pow(R,2) - (3.75*C_9*pow(kn,2))/pow(R,2) + CK_15*(I_0((lambda_4*R)/kn) + I_2((lambda_4*R)/kn)) + CK_16*(K_0((lambda_4*R)/kn) + K_2((lambda_4*R)/kn));
 
@@ -201,8 +114,8 @@ class Pressure : public dolfin::Expression {
         double R   = sqrt(pow(x[0],2)+pow(x[1],2));
         double phi = atan2(x[1],x[0]);
 
-        double d_0 = C_9 + CK_4*I_0((R*lambda_2)/kn) + CK_3*K_0((R*lambda_2)/kn);
-        double d = (4*C_4*R)/kn - (2*C_5*kn)/R + CK_10*I_1((R*lambda_2)/kn) + CK_9*K_1((R*lambda_2)/kn);
+        double d_0 = C_3 - (4*CK_5*I_0((lambda_2*R)/kn))/3. - (4*CK_6*K_0((lambda_2*R)/kn))/3.;
+        double d = (C_7*kn)/R - (2.*C_8*R)/kn - (8*CK_7*I_1((lambda_2*R)/kn))/3. - (8*CK_8*K_1((lambda_2*R)/kn))/3.;
 
         double p = d_0 + cos(phi) * d;
 
@@ -221,7 +134,6 @@ class Velocity : public dolfin::Expression {
 
         double a_0 = (0.5*C_1*kn)/R - (0.4*C_2*kn)/R;
         double a = C_11 + (0.25*C_6*pow(kn,2))/pow(R,2) - (1.5*C_9*pow(kn,2))/pow(R,2) - (0.25*C_8*pow(R,2))/pow(kn,2) + CK_15*((-2*I_0((lambda_4*R)/kn))/5. + (2*I_2((lambda_4*R)/kn))/5.) + CK_16*((-2*K_0((lambda_4*R)/kn))/5. + (2*K_2((lambda_4*R)/kn))/5.) + C_7*(0.5 + (1.6666666666666667*pow(kn,2))/pow(R,2) - log(R/kn)/2.);
-
         double b_0 = (0.5*C_0*kn)/R + (C_5*R)/kn - (2*CK_13*I_1((lambda_4*R)/kn))/5. - (2*CK_14*K_1((lambda_4*R)/kn))/5.;
         double b = C_11 - (0.25*C_6*pow(kn,2))/pow(R,2) + (1.5*C_9*pow(kn,2))/pow(R,2) - (0.75*C_8*pow(R,2))/pow(kn,2) + CK_15*((-2*I_0((lambda_4*R)/kn))/5. - (2*I_2((lambda_4*R)/kn))/5.) + CK_16*((-2*K_0((lambda_4*R)/kn))/5. - (2*K_2((lambda_4*R)/kn))/5.) + C_7*((-1.6666666666666667*pow(kn,2))/pow(R,2) - log(R/kn)/2.);
 
@@ -247,10 +159,8 @@ class Stress : public dolfin::Expression {
 
         double gamma_0 = (C_1*pow(kn,2))/pow(R,2) + CK_5*(I_0((lambda_2*R)/kn)/3. + I_2((lambda_2*R)/kn)) + CK_9*(-I_0((lambda_3*R)/kn) + I_2((lambda_3*R)/kn)) + CK_6*(K_0((lambda_2*R)/kn)/3. + K_2((lambda_2*R)/kn)) + CK_10*(-K_0((lambda_3*R)/kn) + K_2((lambda_3*R)/kn));
         double gamma = (C_6*pow(kn,3))/pow(R,3) + (C_7*kn)/R + (C_8*R)/kn + CK_7*((5*I_1((lambda_2*R)/kn))/3. + I_3((lambda_2*R)/kn)) + CK_11*(-I_1((lambda_3*R)/kn) + I_3((lambda_3*R)/kn)) + CK_8*((5*K_1((lambda_2*R)/kn))/3. + K_3((lambda_2*R)/kn)) + CK_12*(-K_1((lambda_3*R)/kn) + K_3((lambda_3*R)/kn));
-
         double kappa_0 = (C_0*pow(kn,2))/pow(R,2);
         double kappa = (C_6*pow(kn,3))/pow(R,3) - (C_8*R)/kn + CK_7*(-I_1((lambda_2*R)/kn) + I_3((lambda_2*R)/kn)) + CK_11*(-I_1((lambda_3*R)/kn) + I_3((lambda_3*R)/kn)) + CK_8*(-K_1((lambda_2*R)/kn) + K_3((lambda_2*R)/kn)) + CK_12*(-K_1((lambda_3*R)/kn) + K_3((lambda_3*R)/kn));
-
         double omega_0 = (C_1*pow(kn,2))/pow(R,2) + CK_5*(-0.3333333333333333*I_0((lambda_2*R)/kn) + I_2((lambda_2*R)/kn)) + CK_9*(I_0((lambda_3*R)/kn) + I_2((lambda_3*R)/kn)) + CK_6*(-0.3333333333333333*K_0((lambda_2*R)/kn) + K_2((lambda_2*R)/kn)) + CK_10*(K_0((lambda_3*R)/kn) + K_2((lambda_3*R)/kn));
         double omega = (C_6*pow(kn,3))/pow(R,3) + (C_7*kn)/R + (C_8*R)/kn + CK_7*(I_1((lambda_2*R)/kn)/3. + I_3((lambda_2*R)/kn)) + CK_11*(3*I_1((lambda_3*R)/kn) + I_3((lambda_3*R)/kn)) + CK_8*(K_1((lambda_2*R)/kn)/3. + K_3((lambda_2*R)/kn)) + CK_12*(3*K_1((lambda_3*R)/kn) + K_3((lambda_3*R)/kn));
 
